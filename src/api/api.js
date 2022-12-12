21bb36ca1c50124160607204033cef04d698a916
Alejohax <alexander.johanssonn@gmail.com>
2022-12-12 18:08:19 +0100

    Appjs apijs

--- src/api/api.js
+++ src/api/api.js
@@ -7,3 +7,46 @@ async function getAll() {
     .catch((e) => console.log(e));
   return response;
 }
+
+class Api {
+  url = "";
+
+  constructor(url) {
+    this.url = url;
+  }
+
+  create(data) {
+    const JSONData = JSON.stringify(data);
+    console.log(`Sending ${JSONData} to ${this.url}`);
+
+    const request = new Request(this.url, {
+      method: "POST",
+      body: JSONData,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+
+    return fetch(request)
+      .then((result) => result.json())
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  }
+
+  getAll() {
+    return fetch(this.url)
+      .then((result) => result.json())
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  }
+
+  remove(id) {
+    console.log(`Removing ${id} from ${this.url}`);
+
+    return fetch(`${this.url}/${id}`, {
+      method: "DELETE",
+    })
+      .then((result) => result)
+      .catch((err) => console.log(err));
+  }
+}
--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ let pokemonList = [];
 const searchField = document.getElementById("searchField");
 const loadingText = document.getElementById("loading");
 
+const api = new Api("http://localhost:5000/collection");
+
 window.addEventListener("load", () => {
   getAll().then((pokemons) => {
     //localStorage.setItem("pokeList", JSON.stringify(pokemons));
@@ -41,12 +43,34 @@ const renderList = (list) => {
   forms.forEach((form) => {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
-      console.log(e.target[0].value);
-      console.log(e.target.parentNode.parentNode.id);
+      const name =
+        e.target.parentNode.previousSibling.previousSibling.children[0]
+          .innerHTML;
+      const comment = e.target[0].value;
+      const image =
+        e.target.parentNode.previousSibling.previousSibling.children[1].src;
+
+      //console.log(e.target[0].value);
+      console.log(name, comment, image);
+      saveTask(name, comment, image);
       //Hantera put till server här, skicka med value, id .
     });
   });
 };
+
+///test för api save osv
+function saveTask(name, comment, image) {
+  const task = {
+    name: name,
+    comment: comment,
+    image: image,
+  };
+  api.create(task).then((task) => {
+    if (task) {
+      console.log(task);
+    }
+  });
+}
 
 const button = document.getElementById("button");
 
