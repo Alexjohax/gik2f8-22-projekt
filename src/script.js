1c3f9fb160f1cbe35fa3941b9435f5c7c4860b12
Alejohax <alexander.johanssonn@gmail.com>
2022-12-12 19:15:33 +0100

    ny slide

--- src/script.js
+++ src/script.js
@@ -97,9 +97,29 @@ function renderPokemon() {
     /// test för slider
     $(document).ready(function () {
       $(".slideshow").slick({
-        infinite: true,
+        centerMode: true,
+        centerPadding: "60px",
         slidesToShow: 3,
-        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: "40px",
+              slidesToShow: 3,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: "40px",
+              slidesToShow: 1,
+            },
+          },
+        ],
       });
     });
   });
