c33b169211910f5588f448e63c993eed095dfd92
Alejohax <alexander.johanssonn@gmail.com>
2022-12-12 15:36:00 +0100

    First edits in script

--- src/script.js
+++ src/script.js
@@ -1,11 +1,22 @@
 let pokemonList;
+const searchField = document.getElementById("searchField");
+const searchFieldLabel = document.getElementById("searchFieldLabel");
+console.log(searchFieldLabel.innerHTML);
+searchField.setinnerHTML = "Loading...";
 
 window.addEventListener("load", () => {
   getAll().then((pokemons) => {
-    localStorage.setItem("pokeList", JSON.stringify(pokemons));
+    //localStorage.setItem("pokeList", JSON.stringify(pokemons));
     console.log(pokemons);
     pokemonList = pokemons;
+    searchField.classList.remove("hidden");
   });
 });
 
 console.log(pokemonList);
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  console.log(pokemonList);
+});
