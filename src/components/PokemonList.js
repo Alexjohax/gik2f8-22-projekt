f2349a254264c16167c242428a84ca5588d19b8e
Jake <jake.nordvall@hotmail.se>
2022-12-20 22:36:33 +0100

    more pixel stuff

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,5 +1,5 @@
 const PokemonList = (list) => {
-  let html = `<ul id="thelist" class="pokemon-list rounded-md border-2 border-blue-400 bg-white w-full mx-autp">`;
+  let html = `<ul id="thelist" class="pokemon-list pixel-corners rounded-md border-2 border-blue-400 bg-white w-full mx-autp">`;
   for (let i = 0; i < list.length; i++) {
     html += PokemonListItem(list[i]);
   }
--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,6 +1,6 @@
 const PokemonListItem = (pokemon) => {
   let html = `
-      <li id="${pokemon.id}" class="pokemon-list__item font-pixelfont  mb-2 mx-2 last:mb-0 p-3 text-indigo-800 
+      <li id="${pokemon.id}" class="pokemon-list__item text-xl font-pixelfont  mb-2 mx-2 last:mb-0 p-3 text-indigo-800 
       last:border-b-0 border-b border-indigo-500 cursor-pointer flex justify-between items-center">
       <div class="flex-1 ">
       <p class="">${pokemon.name}</p>
@@ -9,7 +9,7 @@ const PokemonListItem = (pokemon) => {
       <div class="flex-1">
       <form class="submitForm flex flex-col">
         <label for="comment">Lägg till en kommentar</label>
-        <input class="border border-red-600" type="text" id="comment" name="comment" placeholder="Skriv något..." />
+        <input class="pixel-corners border-black/20 border-4 text-white text-center  " type="text" id="comment" name="comment" placeholder="Skriv något..." />
         <button class="comment-button pixel-corners    flex justify-center bg-green-400 hover:bg-green-500 text-center text-white  py-2 px-4 rounded-full  mt-4 self-end shadow-md  hover:shadow-lg active:bg-white active:shadow-lg transition duration-150 ease-in-out">Add to collection</button>
       </form>
       </div>
